0ab908c3c7c03b05d49e30461864121a38f14075
Joey Turczak <joeyturczak@gmail.com>
2017-09-11 20:31:34 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Log Analysis
+
+This is a reporting tool for analyzing a news article database. It contains a few reports that display the most popular articles, authors, and days with a significant amount of HTTP request errors.
+
+## Usage
+
+The database and instructions for setting up the environment to run this program can be found [here](https://classroom.udacity.com/nanodegrees/nd004/parts/8d3e23e1-9ab6-47eb-b4f3-d5dc7ef27bf0/modules/bc51d967-cb21-46f4-90ea-caf73439dc59/lessons/262a84d7-86dc-487d-98f9-648aa7ca5a0f/concepts/a9cf98c8-0325-4c68-b972-58d5957f1a91).
+
+Place the python file in the same directory as newsdata.sql and use the command `python newsdata.py` to run the program.
---
+++ newsdata.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import psycopg2
+
+DBNAME = "news"
+
+
+def most_popular_posts():
+    """Return the three most popular articles of all time"""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+
+    c.execute("select title, count(*) as views from articles, log where "
+              "log.path like '%' || articles.slug || '%' group by title "
+              "order by views desc limit 3;")
+    return c.fetchall()
+
+    db.close()
+
+
+def most_popular_authors():
+    """Return the most popular authors of all time"""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+
+    c.execute("select name, count(*) as views from authors, articles, log "
+              "where authors.id = articles.author and log.path like '%' || "
+              "articles.slug || '%' group by name order by views desc;")
+    return c.fetchall()
+
+    db.close()
+
+
+def request_errors():
+    """Return the days on which more than 1% of requests had errors"""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+
+    c.execute("select time, round((errors * 100.0) / total, 1) as error_perc "
+              "from (select time::date as time, count(*) as total, count(case "
+              "when status != '200 OK' then 1 end) as errors from log "
+              "group by time::date) as subq where ((errors * 100) / "
+              "total) > 1;")
+    return c.fetchall()
+
+    db.close()
+
+
+# Get and print out three most popular articles
+print("The three most popular articles of all time: \n")
+rows = most_popular_posts()
+for row in rows:
+    print("%s -- %s views\n" % (row[0], row[1]))
+
+# Get and print out the most popular authors of all time
+print("\nThe most popular authors of all time: \n")
+rows = most_popular_authors()
+for row in rows:
+    print("%s -- %s views\n" % (row[0], str(row[1])))
+
+# Get and print out the days which more than 1% of requests lead to errors
+print("\nThe days which more than 1% of requests lead to errors: \n")
+rows = request_errors()
+for row in rows:
+    print("%s -- %s%% errors\n" % (str(row[0]), str(row[1])))
