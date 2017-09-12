84702104f6ba78613952d69506d0fd2f146bb2c4
Joey Turczak <joeyturczak@gmail.com>
2017-09-11 21:47:45 -0700

    add __name__ block

--- newsdata.py
+++ newsdata.py
@@ -64,21 +64,21 @@ def request_errors():
 
     db.close()
 
+if __name__ == '__main__':
+    # Get and print out three most popular articles
+    print("The three most popular articles of all time: \n")
+    rows = most_popular_posts()
+    for row in rows:
+        print("%s -- %s views\n" % (row[0], row[1]))
 
-# Get and print out three most popular articles
-print("The three most popular articles of all time: \n")
-rows = most_popular_posts()
-for row in rows:
-    print("%s -- %s views\n" % (row[0], row[1]))
+    # Get and print out the most popular authors of all time
+    print("\nThe most popular authors of all time: \n")
+    rows = most_popular_authors()
+    for row in rows:
+        print("%s -- %s views\n" % (row[0], str(row[1])))
 
-# Get and print out the most popular authors of all time
-print("\nThe most popular authors of all time: \n")
-rows = most_popular_authors()
-for row in rows:
-    print("%s -- %s views\n" % (row[0], str(row[1])))
-
-# Get and print out the days which more than 1% of requests lead to errors
-print("\nThe days which more than 1% of requests lead to errors: \n")
-rows = request_errors()
-for row in rows:
-    print("%s -- %s%% errors\n" % (str(row[0]), str(row[1])))
+    # Get and print out the days which more than 1% of requests lead to errors
+    print("\nThe days which more than 1% of requests lead to errors: \n")
+    rows = request_errors()
+    for row in rows:
+        print("%s -- %s%% errors\n" % (str(row[0]), str(row[1])))
